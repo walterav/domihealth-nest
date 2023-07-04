749dd3f4914ee74a4ca0cc3c211b9a21c8dc8d4a
walterav <130625099+walterav@users.noreply.github.com>
2023-07-03 22:02:47 -0500

    creacion tabla clinic-history finalizado

--- src/clinic-history/clinic-history.module.ts
+++ src/clinic-history/clinic-history.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ClinicHistoryService } from './clinic-history.service';
 import { ClinicHistoryController } from './clinic-history.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClinicHistory } from './entities/clinic-history.entity';
 
 @Module({
   controllers: [ClinicHistoryController],
-  providers: [ClinicHistoryService]
+  providers: [ClinicHistoryService],
+  imports: [
+    TypeOrmModule.forFeature([ClinicHistory])
+  ]
 })
 export class ClinicHistoryModule {}
--- src/clinic-history/entities/clinic-history.entity.ts
+++ src/clinic-history/entities/clinic-history.entity.ts
@@ -1,1 +1,53 @@
-export class ClinicHistory {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ClinicHistory {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{
+    })
+    gender:string;
+
+    @Column('text',{
+    })
+    fullName:string;
+
+    @Column('int',{
+        unique: true,
+    })
+    idNumber:number;
+
+    @Column('date',{
+    })
+    dateOfBirth:Date;
+
+    @Column('text',{
+    })
+    address:string;
+
+    @Column('int',{
+    })
+    phoneNumber:number;
+
+    @Column('text',{
+    })
+    familiarContact:string;
+
+    @Column('text',{
+    })
+    profession:string;
+
+    @Column('text',{
+    })
+    maritalStatus:string;
+
+    @Column('text',{
+    })
+    bloodType:string;
+
+    @Column('text',{
+    })
+    religion:string;
+}
