282aad18a0522368e5001e128bd5832502b164eb
walterav <130625099+walterav@users.noreply.github.com>
2023-07-03 22:08:04 -0500

    crear clinic-history dto finalizado

--- src/clinic-history/dto/create-clinic-history.dto.ts
+++ src/clinic-history/dto/create-clinic-history.dto.ts
@@ -1,1 +1,37 @@
-export class CreateClinicHistoryDto {}
+import { IsDateString, IsIn, IsNumber, IsString } from "class-validator";
+
+export class CreateClinicHistoryDto {
+    
+    @IsIn(['male', 'female', 'other'])
+    gender:string;
+
+    @IsString()
+    fullName:string;
+
+    @IsNumber()
+    idNumber:number;
+
+    @IsDateString()
+    dateOfBirth:Date;
+
+    @IsString()
+    address:string;
+
+    @IsNumber()
+    phoneNumber:number;
+
+    @IsString()
+    familiarContact:string;
+
+    @IsString()
+    profession:string;
+
+    @IsIn(['single', 'married', 'divorced', 'widowed', 'common-law'])
+    maritalStatus:string;
+
+    @IsIn(['A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-'])
+    bloodType:string;
+
+    @IsIn(['evangelical-christian', 'catholic', 'protestant', 'atheist', 'other'])
+    religion:string;
+}
